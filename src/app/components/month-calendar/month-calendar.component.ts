8b6111c38f56458d3dc78912e4bd03c5317f536d
Yoann Moreau <600707+ymoreau@users.noreply.github.com>
2024-12-07 22:41:20 +0100

    Allow to navigate to next or previous month

--- src/app/components/month-calendar/month-calendar.component.ts
+++ src/app/components/month-calendar/month-calendar.component.ts
@@ -16,6 +16,7 @@ export class MonthCalendarComponent {
   days: Day[] = [];
   headers = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
   disabledDays = [WeekDay.SATURDAY, WeekDay.SUNDAY];
+  currentMonthLabel = '';
 
   readonly WorkDayStatus = WorkDayStatus; // Alias for HTML template
 
@@ -23,6 +24,7 @@ export class MonthCalendarComponent {
 
   constructor() {
     this.days = this.calendarService.currentMonthDays();
+    this.refreshMonthLabel();
   }
 
   dayColumnPosition(day: Day): number {
@@ -35,5 +37,21 @@ export class MonthCalendarComponent {
 
   workDaysCount(): number {
     return this.calendarService.getWorkedDaysCount(this.days);
+  }
+
+  goToPreviousMonth(): void {
+    this.days = this.calendarService.setToPreviousMonth();
+    this.refreshMonthLabel();
+  }
+
+  goToNextMonth(): void {
+    this.days = this.calendarService.setToNextMonth();
+    this.refreshMonthLabel();
+  }
+
+  private refreshMonthLabel(): void {
+    this.days = this.calendarService.currentMonthDays();
+    const date = this.calendarService.currentDate();
+    this.currentMonthLabel = date.toLocaleString('en-EN', { month: 'long' }) + ' ' + date.getFullYear();
   }
 }
